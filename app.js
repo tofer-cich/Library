e848566d13aa44611a3ab4f55e00fa93b2b425a1
Tofer-Cich <[tofercich@hotmail.com]>
2024-08-24 12:15:12 -0500

    Set up skeleton css, html, and js files
    project is ready to be created.

---
+++ app.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+///////////////////////////////
+///// Book constructor/////////
+///////////////////////////////
+
+function addBookToLibrary() {
+    const title = prompt("What's the name of the book?");
+    const author = prompt("Who wrote it?");
+    const pages = prompt("How many pages is it?");
+    const hasRead = prompt("Have you read it?");
+
+    //create object here
+    //push to library
+}

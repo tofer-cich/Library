bbc52d9850fe00d5479266e69d61e748a34ec96c
Tofer-Cich <[tofercich@hotmail.com]>
2024-08-25 12:43:02 -0500

    Display books dynamically
    now the javascript function properly embeds span elements to the books
    so css can style it accordingly.

--- app.js
+++ app.js
@@ -24,13 +24,37 @@ function createCard(title, author, pages, hasRead) {
     const card = document.createElement('div');
     card.classList.add('card');
 
+    //title
+    const cardTitle = document.createElement('p');
+    const cardTitleHead = document.createElement('span');
+    cardTitleHead.textContent = "Title: ";
+    cardTitle.appendChild(cardTitleHead);
+    cardTitle.appendChild(document.createTextNode(title));
+
     const cardAuthor = document.createElement('p');
     const cardAuthorHead = document.createElement('span');
     cardAuthorHead.textContent = "Author: ";
     cardAuthor.appendChild(cardAuthorHead);
-    cardAuthor.textContent += author;
+    cardAuthor.appendChild(document.createTextNode(author));
 
+    //pages
+    const cardPages = document.createElement('p');
+    const cardPagesHead = document.createElement('span');
+    cardPagesHead.textContent = "Pages: ";
+    cardPages.appendChild(cardPagesHead);
+    cardPages.appendChild(document.createTextNode(pages));
+
+    //hasRead
+    const cardHasRead = document.createElement('p');
+    const cardHasReadTitle = document.createElement('span');
+    cardHasReadTitle.textContent = "Has been read: ";
+    cardHasRead.appendChild(cardHasReadTitle);
+    cardHasRead.appendChild(document.createTextNode(hasRead));
+
+    card.appendChild(cardTitle);
     card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(cardHasRead);
 
     return card;
 }

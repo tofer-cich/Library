ef690f6c5ce2867620872a58061f0a96fe071b11
Tofer-Cich <[tofercich@hotmail.com]>
2024-08-28 14:11:51 -0500

    Create remove and change read status functions
    functions work for remove and read status. it updates the array and
    the displayed cards. could clean up the create card function by using
    the object and it's attributes.

--- app.js
+++ app.js
@@ -8,24 +8,36 @@ function Book(title, author, pages, hasRead) {
     this.pages = pages;
     this.hasRead = hasRead;
 }
+Book.prototype.changeReadStatus = function(target) {
+    if (this.hasRead === "no") {
+        this.hasRead = "yes";
+    } else {
+        this.hasRead = "no";
+    }
+}
 
 const bookContainer = document.querySelector("#book-container");
 
 bookContainer.addEventListener("click", (e) => {
     let target = e.target;
+    let card = target.parentElement;
 
     switch (target.value) {
         case "Remove":
-            removeBookFromLibrary();
+            removeBookFromLibrary(target.parentElement);
             break;
-        case "Read Status":
+        case "Change Read Status":
             console.log("You clicked the read status button");
+            changeReadStatus(card);
             break;
         default:
             console.log("Unfamiliar button pressed");
+            console.log(myLibrary);
     }
     
 });
+
+let count = 1;
 
 function addBookToLibrary() {
     const title = prompt("What's the name of the book?");
@@ -34,15 +46,26 @@ function addBookToLibrary() {
     const hasRead = prompt("Have you read it?");
 
     const newBook = new Book(title, author, pages, hasRead);
+    newBook.id = count++;
     myLibrary.push(newBook);
-    bookContainer.appendChild(createCard(title, author, pages, hasRead));
+    bookContainer.appendChild(createCard(title, author, pages, hasRead, newBook.id, newBook));
 }
 
-function removeBookFromLibrary() {
-    console.log("removeBookFromLibrary");
+function removeBookFromLibrary(target) {
+    let indexToRemove = -1, searchTerm = target.dataset.indexNumber;
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id == searchTerm) {
+            indexToRemove = i;
+            break;
+        }
+    }
+
+    myLibrary.splice(indexToRemove, 1);
+    target.remove();
 }
 
-function createCard(title, author, pages, hasRead) {    
+function createCard(title, author, pages, hasRead, id, book) {    
     const card = document.createElement('div');
     card.classList.add('card');
 
@@ -79,14 +102,19 @@ function createCard(title, author, pages, hasRead) {
     deleteButton.type = "button";
     deleteButton.value = "Remove";
 
+    const changeReadStatusButton = document.createElement('input');
+    changeReadStatusButton.type = "button";
+    changeReadStatusButton.value = "Change Read Status";
+
     card.appendChild(cardTitle);
     card.appendChild(cardAuthor);
     card.appendChild(cardPages);
     card.appendChild(cardHasRead);
     card.appendChild(deleteButton);
+    card.appendChild(changeReadStatusButton);
 
-    card.dataset.indexNumber = myLibrary.length;
-    console.log(card.dataset.indexNumber);
+    card.dataset.indexNumber = id;
+    card.objectData = book;
 
     return card;
 }
@@ -94,3 +122,16 @@ function createCard(title, author, pages, hasRead) {
 newBookButton.addEventListener("click", () => {
     addBookToLibrary();
 });
+
+function changeReadStatus(target) {
+    let node = target.childNodes[3].childNodes[1];
+
+    if (node.nodeValue == "no") {
+        node.nodeValue = "yes";
+    } else {
+        node.nodeValue = "no";
+    }
+
+    target.objectData.changeReadStatus();
+    
+}
